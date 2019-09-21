1e5a2e3ed7aa2f81fa31eb3ed3852121c27dba07
igokuly2k <igokulsmailbox@gmail.com>
2019-09-21 16:04:33 +0530

    Basic Layout Finished Successfully

--- src/gui/PassengerHistory.java
+++ src/gui/PassengerHistory.java
@@ -1,12 +1,11 @@
 package gui;
-
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Vector;
 public class PassengerHistory extends JPanel{
-
 	/**
 	 * 
 	 */
@@ -15,24 +14,24 @@ public class PassengerHistory extends JPanel{
     private JScrollPane sp;
     public PassengerHistory(ResultSet rs) throws SQLException
     {
-    	String column[]={"FROM PLACE","TO PLACE","PRINT TIME"};   
-    	String data[][]=new String[20][3];
-        while (rs.next()) {
-            String em = rs.getString("fromplace");
-            for(int i=0;i<20;i++) {
-           data[i][0]= em.split("\n")[i];}
-           }
-        while (rs.next()) {
-            String em = rs.getString("toplace");
-           for(int j=0;j<20;j++)
-               data[j][1]= em.split("\n")[j];
-           }
-        while (rs.next()) {
-            String em = rs.getString("printdate");
-            for(int k=0;k<20;k++)
-                data[k][2]= em.split("\n")[k];
-           }
-        jt=new JTable(data,column);
+    	rs.next();
+    	Vector <Vector<Object>> data=new Vector<Vector<Object>>();
+    	Vector<String> column=new Vector<String>();
+    	column.add("From Place");
+    	column.add("To Place");
+    	column.add("Time");
+        do
+       {
+            Vector<Object> row = new Vector<Object>();
+
+               row.addElement( rs.getObject("fromplace") );
+               row.addElement( rs.getObject("toplace") );
+               row.addElement(rs.getObject("printdate") );
+               
+
+           data.add( row );
+       }while(rs.next());
+    	jt=new JTable(data,column);
         sp=new JScrollPane(jt); 
         add(sp);
     }
